277708e8ccc615208d7bad6f6cfa8c84d6132a59
Oscar Morales <oscar.s.morales.z@usp.br>
2021-10-14 18:33:00 -0300

    Add index.ejs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Server works');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+
+// routes
+app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index', { title: 'First Web'});
+});
+
+// static files
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
