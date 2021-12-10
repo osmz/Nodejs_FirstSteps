d7e0c787e03648bd3bc82a967efdb52cd7ab2237
Oscar Morales <oscar.s.morales.z@usp.br>
2021-12-10 15:21:12 -0300

    Database change

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,19 @@
+// module.exports = {
+
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'Oscar9395@',
+//         database: 'rey_copas'
+//     }
+// };
+
 module.exports = {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'Oscar9395@',
+        host: 'teste.cxfdqexy7t8d.us-west-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'DSVTeste1',
         database: 'rey_copas'
     }
 };
